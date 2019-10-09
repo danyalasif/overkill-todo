d38ae93117a363e52be89cf84a119746128601af
Danyal <danyal.aasif@gmail.com>
2019-10-09 18:00:06 +0500

    Set up a basic file structure and Readme

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Header = () => {
+  return <header>Hi, I'm a navbar!</header>
+}
+
+export default Header
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TodoList = () => {
+  return <div>I'm a todo list, I embody everything listy</div>
+}
+
+export default TodoList
