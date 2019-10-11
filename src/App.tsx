fd28b75901aac82800f277054ebbd3e891148e42
Danyal <danyal.aasif@gmail.com>
2019-10-11 20:21:38 +0500

    Basic redux files and structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
 import Layout from './components/Layout'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './store/reducers'
+
+const store = createStore(rootReducer)
+
 // import './App.css'
 
 const App: React.FC = () => {
-  return <Layout />
+  return (
+    <Provider store={store}>
+      <Layout />
+    </Provider>
+  )
 }
 
 export default App
